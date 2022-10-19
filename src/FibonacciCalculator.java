19e80228f697d118c5b2630b7de06ef308a79406
ntuff <ndtuff@gmail.com>
2022-10-19 11:23:56 -0230

    .

---
+++ src/FibonacciCalculator.java
@@ -0,0 +1,40 @@
+/**
+ * A class that allows someone to validate and generate fibonacci numbers.
+ *
+ * The Fibonacci numbers, commonly denoted Fn, form a sequence, called the Fibonacci sequence, such
+ * that each number is the sum of the two preceding ones, starting from 0 and 1. That is:
+ * <p>
+ * F_0 = 0 and F_1 = 1
+ * <p>
+ *  And
+ * <p>
+ * F_N = F_N-1 + F_N-2
+ * <p>
+ * The beginning of the sequence is thus: 0, 1, 1, 2, 3, 5, 8, 13, ..
+ * </pre>
+ * @author 20148596
+ */
+
+public class FibonacciCalculator {
+
+    /**
+     * Check to see if an integer passed in is a Fibonacci Number. Warning = Fibonacci numbers get
+     * large fast to be cautous of the size of an integer.
+     * @param number integer to check
+     * @return true if n is a Fibonacci Number
+     */
+    public boolean isFibonacciNumber(int number){
+        //TODO: Write this method
+        return false;
+    }
+
+    /**
+     * Get the nth number in the Fibonacci sequence
+     * @param n the position in the Fibonacci sequence
+     * @return value of the nth Fibonacci number
+     */
+    public int getFibonacciNumber(int n){
+        //TODO: Write this method
+        return 0;
+    }
+}
---
+++ src/PrimeNumberCalculator.java
@@ -0,0 +1,40 @@
+/**
+ * A class that allows someone to determine if an integer is a prime number.
+ *
+ * A natural number (positive Integer) is prime if it’s divisible by only 1 and itself.
+ * <p>
+ * For example:
+ * <p>
+ * 2, 3, 5 and 7 are prime
+ * <p>
+ * but:
+ * <p>
+ * 4, 6, 8 and 9 are not.
+ * <p>
+ * The number 1, by definition, is not prime.
+ * </pre>
+ * @author 20148596
+ */
+
+public class PrimeNumberCalculator {
+    /**
+     *
+     * @param number integer to check
+     * @return true if number is prime
+     */
+    public boolean isPrime(int number){
+        //TODO: Write this method
+        return false;
+    }
+
+    /**
+     *
+     * @param number
+     * @return
+     */
+    public String getUniquePrimeFactorization(int number){
+        //TODO: Write this method
+        return "";
+    }
+
+}
