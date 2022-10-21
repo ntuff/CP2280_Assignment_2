fb9f403fc4ab0575d264e05d55544f324b14092b
ntuff <ndtuff@gmail.com>
2022-10-21 14:11:38 -0230

    .

--- src/FibonacciCalculatorTest.java
+++ src/FibonacciCalculatorTest.java
@@ -5,11 +5,16 @@ public class FibonacciCalculatorTest {
     public static void main(String[] args) {
         System.out.println("Fibonacci Calculator Test");
 
-        //Test 1: set a number and check if perfect square
+        //Test 1: set a number and check too see if it's a perfect square
         FibonacciCalculator fibonacciCalculator = new FibonacciCalculator();
-        System.out.printf("Is perfect square = %b\n", fibonacciCalculator.isPerfectSquare(21));
+
+        //System.out.printf("Is perfect square = %b\n", fibonacciCalculator.isPerfectSquare(21));
         fibonacciCalculator.setNumber(40);
-        System.out.printf("Is perfect square = %b\n", fibonacciCalculator.isPerfectSquare(16));
+        System.out.printf("Is %d ", fibonacciCalculator.number);
+        System.out.printf("a perfect square? %b\n", fibonacciCalculator.isPerfectSquare(40));
+        fibonacciCalculator.setNumber(16);
+        System.out.printf("Is %d ", fibonacciCalculator.number);
+        System.out.printf("a perfect square? %b\n", fibonacciCalculator.isPerfectSquare(16));
 
 
     }
