7566e420106d2d7f1a89eb93e29ec905da5e14e9
ntuff <ndtuff@gmail.com>
2022-10-26 10:57:26 -0230

    .

--- src/FibonacciCalculatorApplication.java
+++ src/FibonacciCalculatorApplication.java
@@ -13,38 +13,25 @@ public class FibonacciCalculatorApplication {
             printMenu();
             //Grab the user input
             c = Character.toUpperCase(input.next().charAt(0));
-
-            System.out.printf("\nEcho: %c", c);
+            //System.out.printf("\nEcho: %c", c);
 
             if(c=='A'){
-//                System.out.printf("\nIn Option %c\n", c);
                 System.out.println("Enter number: ");
-//
-//                int depositAmount = input.nextInt();
-//                account.credit(depositAmount);
-//
-//                printAccountInfo(account);
+                int n = input.nextInt();
+                for(int i = 0; i <= n; i++){
+                    System.out.println(FibonacciCalculator.getFibonacciNumber(i));}
+
+
+
 
             } else  if(c=='B'){
-//                System.out.printf("\nIn Option %c\n", c);
                 System.out.println("Enter number: ");
-//
-//                int withdrawalAmount = input.nextInt();
-//                account.debit(withdrawalAmount);
-//
-//                printAccountInfo(account);
+
 
             } else if (c=='C'){
-//                System.out.printf("\nIn Option %c", c);
-                //number = System.out.println("Enter number: ");
-                //System.out.printf(FibonacciCalculator.isFibonacciNumber(number));
-//                printAccountInfo(account);
-
-            } else if (c=='D'){
-//                System.out.printf("\nIn Option %c", c);
                 System.out.println("Enter number: ");
-//
-//                printAccountInfo(account);
+                int number = input.nextInt();
+                System.out.println(FibonacciCalculator.isFibonacciNumber(number));
             }
 
         }while(c!='Q');
@@ -54,15 +41,10 @@ public class FibonacciCalculatorApplication {
 
     public static void printMenu(){
         System.out.println("\n\nPlease select an option:");
-        System.out.println("(A) Deposit funds");
-        System.out.println("(B) Withdraw funds");
-        System.out.println("(C) Reset balance");
-        System.out.println("(D) todo");
+        System.out.println("(A) Display all fibonacci numbers less than 'n'");
+        System.out.println("(B) Print 'n' fibonacci numbers");
+        System.out.println("(C) Determine if a number is a fibonacci number");
+        //System.out.println("(D) todo");
         System.out.println("(Q) Quit");
-    }
-
-    public static void printIsFibonachiNumberResult(FibonacciCalculator number){
-        //System.out.printf(FibonacciCalculator.isFibonacciNumber(number));
-        //System.out.printf("Account name: %s\nBalance: $%.2f", account.getName(), account.getBalance());
     }
 }
--- src/FibonacciCalculatorTest.java
+++ src/FibonacciCalculatorTest.java
@@ -1,10 +1,10 @@
-
+//TODO: Complete test 4, test for negatives in all
 public class FibonacciCalculatorTest {
 
     public static void main(String[] args) {
         System.out.println("Fibonacci Calculator Test");
 
-        //Test 1: set a number and check to see if it's a perfect square
+        //Test 1: set a number and check to see if it's a fibonacci square
         System.out.println("Test 1:");
         System.out.println(FibonacciCalculator.isFibonacciNumber(23));
         System.out.println(FibonacciCalculator.isFibonacciNumber(40));
@@ -12,10 +12,10 @@ public class FibonacciCalculatorTest {
         System.out.println(FibonacciCalculator.isFibonacciNumber(8));
         System.out.println("");
 
-        //Test 2: set a number and print all fibonacci numbers up to entered value
+        //Test 2: Prints n Fibonacci numbers (ie. Print the first 25 Fibonacci numbers)
         System.out.println("Test 2:");
         int n = 14;
-        for(int i = 0; i <= n; i++){
+        for(int i = 0; i < n; i++){
             System.out.println(FibonacciCalculator.getFibonacciNumber(i));}
 
         //Test 3: set a number to specify a place in the fibonacci sequence and print its value
@@ -24,6 +24,13 @@ public class FibonacciCalculatorTest {
         System.out.println(FibonacciCalculator.getFibonacciNumber(4));
         System.out.println(FibonacciCalculator.getFibonacciNumber(7));
         System.out.println(FibonacciCalculator.getFibonacciNumber(14));
+        System.out.println("");
+
+        //Test 4:  displays all the Fibonacci numbers less than a value input by the user
+        System.out.println("Test 4:");
+        int n2 = 14;
+        for(int i = 0; i < FibonacciCalculator.getFibonacciNumber(n2); i++){
+            System.out.println(FibonacciCalculator.getFibonacciNumber(i));}
 
 
 //        //Test 1: set a number and check to see if it's a perfect square
